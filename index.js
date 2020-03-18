3dcb673094c02b1df7ee40d559e9019beac97e6c
LIXUECHUN <1572185662@qq.com>
2020-03-18 15:02:01 +0800

    register

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ $(function(){
         $vfcodetext = $('#vfcodetext'),
         $submit = $('#submit');
         
+    // 字符验证
+    $name.focusout(function(){
+        if(!validate('#name')) return;
+    })
+    $psw.focusout(function(){
+        if(!validate('#psw')) return;
+    })
+    $tel.focusout(function(){
+        if(!validate('#tel')) return;
+    })
+    $vfcode.focusout(function(){
+        if(!validate('#vfcode')) return;
+    })
+
     //验证码推迟
     $getcode.click(function(){
         $vfcodetext.css('display','block')
@@ -29,24 +43,37 @@ $(function(){
         if(!validate('#name') || !validate('#tel') ||!validate('#psw')||!validate('#vfcode'))return;
     });
 
-    //表单级别
+    //表单级别验证
     function validate(field){
         var $data = $(field),
             $msg = $(field + '-validation-msg');
-            // console.log($msg);
         //validate null
         if($data.val()=== ''){
             $msg.html('不能为空!');
             $data.select();//设置焦点
             return false;
         }
-        // if($msg == $('#name-validation-msg')){
-        //     console("111")
-        // }
+        if(field == '#name'){
+            if(/[^\u4E00-\u9FA5\w]/.test($data.val())){
+                $msg.html('用户名仅支持中英文、数字和下划线,且不能为纯数字');
+                return false;
+            }else if(!(/\D/.test($data.val()))){
+                $msg.html('用户名仅支持中英文、数字和下划线,且不能为纯数字');
+                return false;
+            }
+        }
+        else if(field == '#tel'){
+            if(!(/^1[34578]\d{9}$/.test($data.val()))){
+                $msg.html('手机号码格式不正确');
+                return false;
+            }
+        }else if(field == '#psw'){
+            if(!(/(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^[^\s\u4e00-\u9fa5]{8,14}$/.test($data.val()))){
+                $msg.html('密码长度为8-14个字符,数字,字母,字符至少包含两种,不能包含中文和空格');
+                return false;
+            }
+        }
         $msg.html('');
         return true;
     }
-        // 手机字符
-
-    //字符级别
 })
